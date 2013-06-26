404ff9eff5c9de3b86cf554d766f70b0685df462
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2013-06-26 18:21:10 +0200

    add new albums to river

--- actions/edit/object/hjalbum.php
+++ actions/edit/object/hjalbum.php
@@ -7,6 +7,16 @@ if ($result) {
 
 	$images = hj_gallery_handle_uploaded_files($entity);
 
+	foreach ($images as $image) {
+		$image_guids[] = $image->guid;
+	}
+
+	if ($image_guids) {
+		$posted = time();
+		$entity->$posted = serialize($image_guids);
+		add_to_river('river/object/hjalbum/create', 'create', $entity->owner_guid, $entity->guid, $entity->access_id, $posted);
+	}
+
 	if (elgg_is_xhr()) {
 		print json_encode(array('guid' => $entity->guid, 'images' => $images));
 	}
