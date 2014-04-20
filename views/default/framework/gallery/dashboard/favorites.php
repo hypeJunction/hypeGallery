3dfa3ad203133b6302bb070111eece94726411b3
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-04-20 20:01:42 +0200

    Show both albums and images in the favorites tab

--- views/default/framework/gallery/dashboard/favorites.php
+++ views/default/framework/gallery/dashboard/favorites.php
@@ -4,46 +4,21 @@ namespace hypeJunction\Gallery;
 
 $page_owner = elgg_get_page_owner_entity();
 
-$display = get_input('display', 'albums');
-
 echo '<div id="gallery-dashboard-favorites">';
 
-switch ($display) {
-
-	default :
-	case 'albums' :
-		echo elgg_list_entities_from_annotations(array(
-			'types' => 'object',
-			'subtypes' => array('hjalbum'),
-			'annotation_names' => array('likes'),
-			'annotation_owner_guids' => $page_owner->guid,
-			'full_view' => false,
-			'list_type' => get_input('list_type', 'gallery'),
-			'list_type_toggle' => true,
-			'gallery_class' => 'gallery-photostream',
-			'pagination' => true,
-			'limit' => get_input('limit', 20),
-			'offset' => get_input('offset-albums', 0),
-			'offset_key' => 'offset-albums'
-		));
-		break;
-
-	case 'photostream' :
-		echo elgg_list_entities_from_annotations(array(
-			'types' => 'object',
-			'subtypes' => array('hjalbumimage'),
-			'annotation_names' => array('likes'),
-			'annotation_owner_guids' => $page_owner->guid,
-			'list_type' => get_input('list_type', 'gallery'),
-			'list_type_toggle' => true,
-			'gallery_class' => 'gallery-photostream',
-			'full_view' => false,
-			'pagination' => true,
-			'limit' => get_input('limit', 20),
-			'offset' => get_input('offset-photostream', 0),
-			'offset_key' => 'offset-photostream'
-		));
-		break;
-}
+echo elgg_list_entities_from_annotations(array(
+	'types' => 'object',
+	'subtypes' => array('hjalbum', 'hjalbumimage'),
+	'annotation_names' => array('likes'),
+	'annotation_owner_guids' => $page_owner->guid,
+	'full_view' => false,
+	'list_type' => get_input('list_type', 'gallery'),
+	'list_type_toggle' => true,
+	'gallery_class' => 'gallery-photostream',
+	'pagination' => true,
+	'limit' => get_input('limit', 20),
+	'offset' => get_input('offset-albums', 0),
+	'offset_key' => 'offset-albums'
+));
 
 echo '</div>';
