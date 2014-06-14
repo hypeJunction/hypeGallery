9ccd837f661755821e8aa112529d9d7ac6c37e33
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-06-14 11:23:43 +0200

    Move constants to a settings file

--- start.php
+++ start.php
@@ -31,24 +31,7 @@ require_once __DIR__ . '/lib/functions.php';
 require_once __DIR__ . '/lib/events.php';
 require_once __DIR__ . '/lib/hooks.php';
 require_once __DIR__ . '/lib/page_handlers.php';
-
-define('HYPEGALLERY_RELEASE', 1374851653);
-
-define('HYPEGALLERY_ALBUM_RIVER', elgg_get_plugin_setting('album_river', PLUGIN_ID));
-define('HYPEGALLERY_FAVORITES', elgg_get_plugin_setting('favorites', PLUGIN_ID));
-define('HYPEGALLERY_INTERFACE_LOCATION', elgg_get_plugin_setting('interface_location', PLUGIN_ID));
-define('HYPEGALLERY_INTERFACE_CALENDAR', elgg_get_plugin_setting('interface_calendar', PLUGIN_ID));
-define('HYPEGALLERY_COPYRIGHTS', elgg_get_plugin_setting('copyrights', PLUGIN_ID));
-define('HYPEGALLERY_CATEGORIES', elgg_get_plugin_setting('categories', PLUGIN_ID));
-define('HYPEGALLERY_COLLABORATIVE_ALBUMS', elgg_get_plugin_setting('collaborative_albums', PLUGIN_ID));
-define('HYPEGALLERY_GROUP_ALBUMS', elgg_get_plugin_setting('group_albums', PLUGIN_ID));
-define('HYPEGALLERY_AVATARS', elgg_get_plugin_setting('avatars', PLUGIN_ID));
-define('HYPEGALLERY_TAGGING', elgg_get_plugin_setting('tagging', PLUGIN_ID));
-define('HYPEGALLERY_DOWNLOADS', elgg_get_plugin_setting('downloads', PLUGIN_ID));
-define('HYPEGALLERY_PUBLIC_DOWNLOADS', elgg_get_plugin_setting('public_downloads', PLUGIN_ID));
-define('HYPEGALLERY_EXIF', elgg_get_plugin_setting('exif', PLUGIN_ID));
-
-define('HYPEGALLERY_ALBUM_IMAGE_QUOTA', 100);
+require_once __DIR__ . '/lib/settings.php';
 
 elgg_set_config('gallery_icon_sizes', array(
 	'800x200' => array(
