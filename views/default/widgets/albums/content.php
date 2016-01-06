039678fed7c7899a6e114a65ff688cbb3a183d02
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-01-06 12:24:49 +0100

    fix(widgets): widget now works in group context

--- views/default/widgets/albums/content.php
+++ views/default/widgets/albums/content.php
@@ -4,11 +4,25 @@ namespace hypeJunction\Gallery;
 
 $entity = elgg_extract('entity', $vars);
 $owner = $entity->getOwnerEntity();
+if (elgg_in_context('dashboard')) {
+	$owner_guids = ELGG_ENTITIES_ANY_VALUE;
+	$container_guids = ELGG_ENTITIES_ANY_VALUE;
+	$more_url = "/gallery";
+} else if ($owner instanceof ElggUser) {
+	$owner_guids = $owner->guid;
+	$container_guids = ELGG_ENTITIES_ANY_VALUE;
+	$more_url = "/gallery/dahsboard/owner/$owner->username?display=albums";
+} else {
+	$owner_guids = ELGG_ENTITIES_ANY_VALUE;
+	$container_guids = $owner->guid;
+	$more_url = "/gallery/group/$owner->guid";
+}
 
 $options = array(
 	'types' => 'object',
 	'subtypes' => array(hjAlbum::SUBTYPE),
-	'owner_guids' => (!elgg_in_context('dashboard')) ? $owner->guid : null,
+	'owner_guids' => $owner_guids,
+	'container_guids' => $container_guids,
 	'limit' => $entity->num_display,
 	'count' => true,
 	'full_view' => false,
@@ -21,13 +35,8 @@ $content = elgg_list_entities($options);
 echo $content;
 
 if ($content) {
-	if (elgg_in_context('dashboard')) {
-		$url = "gallery";
-	} else {
-		$url = "gallery/dahsboard/owner/" . $owner->username . "?display=albums";
-	}
 	$more_link = elgg_view('output/url', array(
-		'href' => $url,
+		'href' => $more_link,
 		'text' => elgg_echo('gallery:widget:more'),
 		'is_trusted' => true,
 	));
